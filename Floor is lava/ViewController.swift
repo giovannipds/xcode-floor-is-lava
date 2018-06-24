8ae470a7922ac5a120318a2c62d0db8223644bed
Giovanni Pires da Silva <giovanni@grupoezoom.com.br>
2018-06-24 19:43:01 -0300

    Detect basic horizontal planes

--- Floor is lava/ViewController.swift
+++ Floor is lava/ViewController.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import ARKit
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.configuration.planeDetection = .horizontal
+        self.sceneView.session.run(configuration)
+        self.sceneView.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +26,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        print("new flat surface detected, new ARPlaneAnchor added")
+        
+    }
 
 }
 
