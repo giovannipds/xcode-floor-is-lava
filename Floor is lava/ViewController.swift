808555bc213495a7a0ae64b6388b9138fd7407c2
Giovanni Pires da Silva <giovanni@grupoezoom.com.br>
2018-06-24 20:13:59 -0300

    Add a lava node

--- Floor is lava/ViewController.swift
+++ Floor is lava/ViewController.swift
@@ -18,13 +18,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         self.configuration.planeDetection = .horizontal
         self.sceneView.session.run(configuration)
         self.sceneView.delegate = self
-        
+        let lavaNode = createLava()
+        self.sceneView.scene.rootNode.addChildNode(lavaNode)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func createLava() -> SCNNode {
+        let lavaNode = SCNNode(geometry: SCNPlane(width: 1, height: 1))
+        lavaNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "lava")
+        lavaNode.position = SCNVector3(0,0,-1)
+        return lavaNode
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
