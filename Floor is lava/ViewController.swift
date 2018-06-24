b705b156220d4a09a89dfce120bd1d418d52a605
Giovanni Pires da Silva <giovanni@grupoezoom.com.br>
2018-06-24 20:44:19 -0300

    Create lava node on detected horizontal planes

--- Floor is lava/ViewController.swift
+++ Floor is lava/ViewController.swift
@@ -18,8 +18,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         self.configuration.planeDetection = .horizontal
         self.sceneView.session.run(configuration)
         self.sceneView.delegate = self
-        let lavaNode = createLava()
-        self.sceneView.scene.rootNode.addChildNode(lavaNode)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -28,17 +26,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func createLava() -> SCNNode {
-        let lavaNode = SCNNode(geometry: SCNPlane(width: 1, height: 1))
+    func createLava(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let lavaNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z)))
         lavaNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "lava")
         lavaNode.geometry?.firstMaterial?.isDoubleSided = true
-        lavaNode.position = SCNVector3(0,0,-1)
+        lavaNode.position = SCNVector3(planeAnchor.center.x,planeAnchor.center.y,planeAnchor.center.z)
         lavaNode.eulerAngles = SCNVector3(90.degreesToRadians, 0, 0)
         return lavaNode
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        let lavaNode = createLava(planeAnchor: planeAnchor)
+        node.addChildNode(lavaNode)
         print("new flat surface detected, new ARPlaneAnchor added")
         
     }
